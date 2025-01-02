490474fd30b21dc0ebf785e8883532f69167cdda
Mateo Lampasona <mateolampasona7@gmail.com>
2025-01-02 15:57:39 -0300

    ELM 60 MATEO
    Creacion de la ruta para poder recibir el form de Newsletter desde el front, y guardar los mails en una DB

--- back/src/forms/form.service.ts
+++ back/src/forms/form.service.ts
@@ -4,11 +4,13 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { ContactForm } from "./entity/formContact.entity";
 import { Repository } from "typeorm";
 import { sendContactFormToAdmins } from "src/Config/nodeMailer";
+import { MailDto } from "src/Users/dto/createUser.dto";
+import { NewsLetter } from "./entity/NewsLetter.entity";
 
 @Injectable()
 export class FormService {
-  
-  constructor(@InjectRepository(ContactForm) private readonly contactFormRepository: Repository<ContactForm>) {}
+  constructor(@InjectRepository(ContactForm) private readonly contactFormRepository: Repository<ContactForm>,
+@InjectRepository(NewsLetter) private readonly newsLetterRepository: Repository<NewsLetter>) {}
 
   async contactForm(data: ContactFormDto) {
     const {name, email, subject, message} = data;
@@ -26,4 +28,33 @@ export class FormService {
     
 
   }
+
+ async  newsLetterForm(data: MailDto) {
+    const {email} = data;
+    if (!email) {
+      throw new Error('Email field is required');
+    }
+    await this.newsLetterRepository.create(data);
+    const savedForm = await this.newsLetterRepository.save(data);
+
+    return {message:"Form saved successfully", savedForm};
+  }
+
+  async getContactForm() {
+    const forms = await this.contactFormRepository.find();
+    if(!forms){
+      throw new Error('No forms found');
+    }
+    return forms
+  }
+  
+  async getNewsLetterForm() {
+    const forms = await this.newsLetterRepository.find();
+    if(!forms){
+      throw new Error('No forms found');
+    }
+    return forms
+  }
+
+
 }
